201397f1abe9cfcc3e32d7e3bb68496397aa7cfd
Bruno Sajermann <bruno.sajermann@accenture.com>
2026-06-19 08:28:12 -0300

    feat: add vite pages plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import Pages from 'vite-plugin-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(),
+    Pages(),
   ],
   resolve: {
     alias: {
